ef598db4a7ea45e491e5ce658c5748b1b21dcb84
Cesar Jr Rodriguez <cesarodriguez4@gmail.com>
2017-03-19 16:32:51 -0400

    Updating home

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,19 +9,24 @@
  */
 angular.module('app')
   .controller('home', function () {
-    var vm = this; 
+    var vm = this;
 
     vm.a1 = "Lideres en Refrigeracion";
     vm.a1p = "Es considerada hoy por hoy la empresa de Almacenamiento Congelado y refrigerado de más rápido crecimiento en el centro del país y la empresa de Transporte Refrigerado más grande de Venezuela.";
-    
+    vm.a = vm.a1;
+
     vm.b1 = "Transportamos tu mercancia";
     vm.b1p = "Contamos con servicio de transporte para llevar tu mercancia a donde necesites";
+    vm.b = vm.b1;
 
     vm.c1 = "Ubicacion";
     vm.c1p = "Oficinas Administrativas y de Operaciones/logística ubicadas en Ctra  Nacional   Valencia-Campo de Carabobo,  Local parcelas Nro 14, 15 y 16  Sector  Parque   Agrinco-Manzana A-27, Tocuyito  Carabobo con 2000 metros de construcción terminada, modernamente equipada.";
-    
+    vm.c = vm.c1;
+
     vm.d1 =  "Personal de primer nivel";
     vm.d1p = "Además contamos con un excelente personal, Obrero, Técnico, Administrativo y Gerencial, capaz de reaccionar inmediatamente ante cualquier reto.";
+    vm.d = vm.d1;
+
   })
   .controller('Empresa', function() {
   	this.hey = "Hey";
