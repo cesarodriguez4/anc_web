7119649d95ea5803500a9f269f6be6aa958e5d0d
Cesar Jr Rodriguez <cesarodriguez4@gmail.com>
2017-04-04 12:45:14 -0400

    ultimos cambios

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -12,10 +12,22 @@ angular.module('app')
     var vm = this;
 
     vm.title = [
-    "En Agronacional del Centro refrigeramos tu mercancía",
-    "Empresa líder en refrigeración y transporte en Carabobo", 
-    "Nuestros clientes así lo confirman", 
-    "Contáctanos al 0241-4511743"
+    {
+      text:"En Agronacional del Centro refrigeramos tu mercancía",
+      image: "../../images/galeria/095.JPG"
+    },
+    {
+      text: "Empresa líder en refrigeración y transporte en Carabobo",
+      image: "../../images/086.JPG"
+    }, 
+    {
+      text: "Refrigera tu producto aquí",
+      image: "../../images/galeria/092.JPG"
+     }, 
+    {
+      text: "Contáctanos al 0241-4511743", 
+      image: "../../images/galeria/gerente.jpg"
+    }
     ]; 
 
     vm.i = 0;
@@ -35,25 +47,25 @@ angular.module('app')
 
     //Contenido de los cuadros con imagenes
     vm.a1 = {
-      text: "Lideres en Refrigeracion",
-      bg: "url('../images/avenues/091.JPG')"
+      text: "LÍDERES EN REFRIGERACIÓN",
+      bg: "url('../images/avenues/093.JPG')"
     };
     vm.a1p = { text:"Es considerada hoy por hoy la empresa de Almacenamiento Congelado y refrigerado de más rápido crecimiento en el centro del país y la empresa de Transporte Refrigerado más grande de Venezuela.",
               bg: "white"};
     vm.a = vm.a1;
-    vm.b1 = {text:"Transportamos tu mercancia", bg: "url('../images/avenues/trans.jpg')"};
     
+    vm.b1 = {text:"TRANSPORTAMOS TU MERCANCÍA", bg: "url('../images/avenues/camion.jpg')"};
     vm.b1p = { text:"Contamos con servicio de transporte para llevar tu mercancia a donde necesites",
     bg: "white"};
     vm.b = vm.b1;
 
-    vm.c1 = {text: "Ubicacion", bg: "url('../images/avenues/maps.jpg')"};
+    vm.c1 = {text: "UBICACIÓN", bg: "url('../images/avenues/maps.jpg')"};
     vm.c1p = {text:"Oficinas Administrativas y de Operaciones/logística ubicadas en Ctra  Nacional   Valencia-Campo de Carabobo,  Local parcelas Nro 14, 15 y 16  Sector  Parque   Agrinco-Manzana A-27, Tocuyito  Carabobo con 2000 metros de construcción terminada, modernamente equipada.",
     bg: "white"};
     vm.c = vm.c1;
 
-    vm.d1 = { text:"Personal de primer nivel",
-    bg: "url('../images/avenues/traba.jpg')"};
+    vm.d1 = { text:"PERSONAL DE PRIMER NIVEL",
+    bg: "url('../images/avenues/097.jpg')"};
     vm.d1p = { text: "Además contamos con un excelente personal, Obrero, Técnico, Administrativo y Gerencial, capaz de reaccionar inmediatamente ante cualquier reto.",
     bg: "white"};
     vm.d = vm.d1;
